a84ff7b57fe2ce35ddf9e86bf2ab8e787f3b067b
Ashu-45 <ashutoshgoswami24@hotmail.com>
2025-02-25 09:48:10 +0530

    Add calculator functionality with basic operations and looping options

--- pkg/calculator_v1.0.c
+++ pkg/calculator_v1.0.c
@@ -9,10 +9,71 @@ int one();
 void calc(){
     me();
     char x_calc;
-    float a,b,c;
     printf("Choose One Option:\n");
     printf("1> one opration\n");
     printf("2> Loop (same opraton but unlimated time)\n");
-    printf("ctrl + a > Back \n>> ");
+    printf("0> Back \n>> ");
     scanf("%s",&x_calc);
+    switch (x_calc){
+        case '1':
+            one();
+            break;
+        case '2':
+            loop();
+            break;
+        case '0':
+            main();
+            break;
+        default:
+            printf("\nPlease enter a correct option.\n");
+            calc();
+            break;
+    }
+}
+
+int one(){
+    char opr;
+    float num1,num2,ans;
+    me();
+    printf("Choose One Option:\n");
+    printf("1> Addition\n");
+    printf("2> Subtraction\n");
+    printf("3> Multiplication\n");
+    printf("4> Division\n");
+    printf(">> ");
+    scanf("%s",&opr);
+    me();
+    printf("Enter 1st Number: ");
+    scanf("%f",&num1);
+    printf("Enter 2nd Number: ");
+    scanf("%f",&num2);
+
+    switch (opr)
+    {
+    case '1':
+        ans = num1 + num2;
+        printf("%.2f + %.2f = %.2f\n",num1,num2,ans);
+        break;
+    case '2':
+        ans = num1 - num2;
+        printf("%.2f - %.2f = %.2f\n",num1,num2,ans);
+        break;
+    case '3':
+        ans = num1 * num2;
+        printf("%.2f * %.2f = %.2f\n",num1,num2,ans);
+        break;
+    case '4':
+        ans = num1 / num2;
+        printf("%.2f / %.2f = %.2f\n",num1,num2,ans);
+        break;
+    
+    default:
+        break;
+    }
+    ashu_exit();
+    calc();
+}
+
+int loop(){
+    
 }
